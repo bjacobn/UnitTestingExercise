15dfba4747f11d81fd0bd7700283c5907b6e2e3f
Jacob <bjacobcode@gmail.com>
2022-01-05 15:58:21 -0800

    intial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,95 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(10, 7, 1, 18)]
+        [InlineData(2, 4, 1, 7)]
+        [InlineData(1, -1, 2, 2)]
+        [InlineData(0, 0, 0, 0)]
         public void Add(int num1, int num2, int num3, int expected)
         {
-            //Arrange
+            // Arrange
+            var test = new UnitTestMethods();
 
-            //Act
+            // Act
+            var actual = test.Add(num1, num2, num3);
 
-            //Assert
-
+            // Assert
+            Assert.Equal(expected, actual);          
         }
 
+
         [Theory]
-        [InlineData()]
+        [InlineData(10, 7, 3)]
+        [InlineData(2, 1, 1)]
+        [InlineData(3, 6, -3)]
+        [InlineData(0, 0, 0)]
+
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
+            var actual = test.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
+        
         [Theory]
-        [InlineData()]
-        public void Multiply(int num1, int num2, int expected)
+        [InlineData(2, 2, 4)]
+        [InlineData(1, 0, 0)]
+        [InlineData(3, -1, -3)]
+        [InlineData(0, 0, 0)]
+
+        public void Multiply(int factor1, int factor2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Multiply(factor1, factor2);
             //Assert
-
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [InlineData(2, 2, 1)]
+        [InlineData(10, 5, 2)]
+        [InlineData(5, 2, 2)]
+        [InlineData(1, 1, 1)]
+        public void Divide(int dividend, int divisor, int expected)
+        {
+            //Arrange
+            var test = new UnitTestMethods();
+            //Act
+            var actual = test.Divide(dividend, divisor);
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
-        [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
+
+        [Fact]
+        public void VerifyName()
         {
-            //Arrange
+            //arrange  
+            var test = new UnitTestMethods();
 
-            //Act
+            //act   
+            var actual = test.LastName();
 
-            //Assert
-
+            //assert   
+            Assert.Equal("Jones", actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void VerifyLastFourSSN()
         {
-            //Arrange
-
-            //Act
-
-            //Assert
-
-        }
-
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
+            //arrange
+            var database = new UnitTestMethods();
+            //act
+            var actual = database.LastFourSSN();
+            //assert
+            Assert.Equal(9558, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,24 +3,53 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // DONE -Make sure your references are set properly
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply (int factor1, int factor2)
+        {
+            return factor1 * factor2;
+        }
+
 
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int dividend, int divisor)
+        {
+        
+            return dividend / divisor;
+            
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string LastName()
+        {
+            string name = "Jones";
+            return name;
+        }
 
-        
+        public int LastFourSSN()
+        {
+            int lastFour  = 9558;
+            return lastFour;
+        }
     }
 }
